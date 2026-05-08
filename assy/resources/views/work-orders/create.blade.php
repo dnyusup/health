17a44b7fddaca8ff3eb60b0d9781552ba1af1778
Deni Yusup <deni.yusup@bekaert.com>
2026-05-08 11:05:39 +0700

    update excel download

--- assy/resources/views/work-orders/create.blade.php
+++ assy/resources/views/work-orders/create.blade.php
@@ -395,7 +395,6 @@ document.addEventListener('click', function(e) {
     </div>
 </div>
 
-<script src="https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js"></script>
 <script>
 let html5QrCode = null;
 let scanTarget = null;
